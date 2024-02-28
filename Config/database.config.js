0176d0f2e4c08f985721ee1eea2982fb918ade2f
tarakeswar jana <107502706+Tarakeswarjana@users.noreply.github.com>
2024-02-28 10:12:24 +0530

    Update database.config.js

--- Config/database.config.js
+++ Config/database.config.js
@@ -1,4 +1,3 @@
 module.exports = {
-    // url: 'mongodb://localhost:27017/crud-node-express'
-    url: 'mongodb+srv://tarakeswarjana0808:123@cluster0.hb768mz.mongodb.net/?retryWrites=true&w=majority'
+
 }
